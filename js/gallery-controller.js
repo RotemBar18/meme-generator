d50513ad35440eaa24d5e360a92c045d81a95a25
Rotem Bar <rotem68907@gmail.com>
2021-04-19 22:51:12 +0300

    fix all fixes FML

--- js/gallery-controller.js
+++ js/gallery-controller.js
@@ -1,20 +1,20 @@
 'use strict'
 const gImgs = [
-    { id: 1, url: '../imgs/1.jpg', keywords: ['happy'] },
-    { id: 2, url: '../imgs/2.jpg', keywords: ['happy'] },
-    { id: 3, url: '../imgs/3.jpg', keywords: ['happy'] },
-    { id: 4, url: '../imgs/4.jpg', keywords: ['happy'] },
-    { id: 5, url: '../imgs/5.jpg', keywords: ['happy'] },
-    { id: 6, url: '../imgs/6.jpg', keywords: ['happy'] },
-    { id: 7, url: '../imgs/7.jpg', keywords: ['happy'] },
-    { id: 8, url: '../imgs/8.jpg', keywords: ['happy'] },
-    { id: 9, url: '../imgs/9.jpg', keywords: ['happy'] },
-    { id: 10, url: '../imgs/10.jpg', keywords: ['happy'] },
-    { id: 11, url: '../imgs/11.jpg', keywords: ['happy'] },
-    { id: 12, url: '../imgs/12.jpg', keywords: ['happy'] },
-    { id: 13, url: '../imgs/13.jpg', keywords: ['happy'] },
-    { id: 14, url: '../imgs/14.jpg', keywords: ['happy'] },
-    { id: 15, url: '../imgs/15.jpg', keywords: ['happy'] }
+    { id: 1, url: 'imgs/1.jpg', keywords: ['happy'] },
+    { id: 2, url: 'imgs/2.jpg', keywords: ['happy'] },
+    { id: 3, url: 'imgs/3.jpg', keywords: ['happy'] },
+    { id: 4, url: 'imgs/4.jpg', keywords: ['happy'] },
+    { id: 5, url: 'imgs/5.jpg', keywords: ['happy'] },
+    { id: 6, url: 'imgs/6.jpg', keywords: ['happy'] },
+    { id: 7, url: 'imgs/7.jpg', keywords: ['happy'] },
+    { id: 8, url: 'imgs/8.jpg', keywords: ['happy'] },
+    { id: 9, url: 'imgs/9.jpg', keywords: ['happy'] },
+    { id: 10, url: 'imgs/10.jpg', keywords: ['happy'] },
+    { id: 11, url: 'imgs/11.jpg', keywords: ['happy'] },
+    { id: 12, url: 'imgs/12.jpg', keywords: ['happy'] },
+    { id: 13, url: 'imgs/13.jpg', keywords: ['happy'] },
+    { id: 14, url: 'imgs/14.jpg', keywords: ['happy'] },
+    { id: 15, url: 'imgs/15.jpg', keywords: ['happy'] }
 ];
 
 
